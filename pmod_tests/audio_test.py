50cd0049ee8f630885f0f4f70b72a44d977a4933
Mike Bell <mdb036@gmail.com>
2024-08-31 19:51:43 +0100

    Add TT Pmod tests

---
+++ pmod_tests/audio_test.py
@@ -0,0 +1,39 @@
+import time
+import machine
+import gc
+import random
+from machine import PWM, Pin
+
+# Set this to flase if no TT carrier is mounted on the demoboard
+DISABLE_TT_ASIC = False
+
+if DISABLE_TT_ASIC:
+    from ttboard.mode import RPMode
+    from ttboard.demoboard import DemoBoard
+    
+def disable_tt_board():
+    if DISABLE_TT_ASIC:
+        # Select the chip ROM, which should always be present and set the bidirs to all inputs
+        # so we can drive them with SPI
+        tt = DemoBoard()
+        tt.shuttle.tt_um_chip_rom.enable()
+        
+def play_audio():
+    pwm = PWM(Pin(28), freq = 700, duty_u16=32768)
+    time.sleep(1)
+    
+    def freq_note(m):
+        return 440*2**((m-69)/12)
+    
+    for i in range(36, 85):
+        pwm.freq(int(freq_note(i)))
+        time.sleep(0.2)
+    for i in range(84, 35, -1):
+        pwm.freq(int(freq_note(i)))
+        time.sleep(0.2)
+
+    time.sleep(0.4)
+    pwm.deinit()
+
+disable_tt_board()
+play_audio()
